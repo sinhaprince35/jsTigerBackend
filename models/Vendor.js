2782813e766a72c463803914fa6b9234b2567c50
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2024-01-22 17:18:08 +0530

    Backend

--- models/Vendor.js
+++ models/Vendor.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: "String",
-  accountNo: "String",
+  accountNo: "Number",
   bankName: "String",
   address1: "String",
   address2: "String",
   city: "String",
   country: "String",
-  zipCode: "String",
+  zipCode: "Number"
 });
 
 module.exports = mongoose.model("users", userSchema);
